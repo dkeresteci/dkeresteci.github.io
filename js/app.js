544368f33e257f02fa94f07c979be956e63a98d4
dkeresteci <dkeresteci@gmail.com>
2014-03-12 20:50:42 -0400

    Added files from home folder

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+//$(document).foundation();
+        $(document).foundation({
+              orbit: {
+                animation: 'slide',
+                timer_speed: 10000,
+                pause_on_hover: false,
+                animation_speed: 500,
+                navigation_arrows: true,
+                slide_number: false,
+                timer: false,
+                bullets: true,
+                next_on_click: true
+
+              }
+            });
